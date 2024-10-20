9b8d9545ad2960e6225a3287cc3642e980706df8
JxmyKara” <d.s.jxmy@gmail.com>
2024-10-20 21:57:19 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -12,6 +12,24 @@ prog:
 #-----------------------------
 # Write your code here!
 # Do not remove the prog label or write code above it!
+    li s0, 0
+    li t1, 0
+    
+loop:
+    beq t1, a1, done 
+    slli t2, t1, 2
+    add t3, a0, t2
+    lw t4, 0(t3)
+    beq t4, a2, update_s0
+    j next
+    
+update_s0:
+    add s0, zero, t3
+
+next:
+    addi t1, t1, 1
+    j loop
+
 #-----------------------------
 done:
     addi a7, zero, 10 
